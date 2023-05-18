357dd1e3f8c754c7a3d007da09add4fc43e0e0ac
Ariana Williams <Ariana.Williams@tlgcohort.com>
2023-05-18 11:23:40 -0600

    Lab05.1 (enum singleton) complete

---
+++ Lab05.1/src/gov/irs/IRS.java
@@ -0,0 +1,12 @@
+package gov.irs;
+
+public interface IRS {
+
+    public void collectTaxes();
+    public void register(TaxPayer payer);
+
+    // static factory method for clients to obtain the SOLE instance of IRSEnum
+    public static IRS getInstance() {
+        return IRSEnum.INSTANCE;
+    }
+}
