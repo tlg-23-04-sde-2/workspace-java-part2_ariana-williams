cd394f047951cbc6aae492249c2b047b4c71c621
Ariana Williams <Ariana.Williams@tlgcohort.com>
2023-05-22 09:10:14 -0600

    compare and comparator complete

--- Scratch/src/com/crisp/RadishSortTest.java
+++ Scratch/src/com/crisp/RadishSortTest.java
@@ -1,6 +1,7 @@
 package com.crisp;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 class RadishSortTest {
@@ -19,7 +20,26 @@ class RadishSortTest {
         radishes.sort(new RadishColorComparator());
         System.out.println("Sort by (color)");
         dump(radishes);
+        System.out.println();
 
+        System.out.println("Sort by tailLength, via anonymous Comparator");
+        radishes.sort(new Comparator<Radish>() {
+            @Override
+            public int compare(Radish r1, Radish r2) {
+                return Double.compare(r1.getTailLength(), r2.getTailLength());
+            }
+        } );
+        dump(radishes);
+        System.out.println();
+
+        System.out.println("Sort by guys on top, via anonymous Comparator");
+        radishes.sort(new Comparator<Radish>() {
+            @Override
+            public int compare(Radish r1, Radish r2) {
+                return Integer.compare(r1.getGuysOnTop(), r2.getGuysOnTop());
+            }
+        });
+        dump(radishes);
     }
 
     private static void dump(List<Radish> radishList) {
