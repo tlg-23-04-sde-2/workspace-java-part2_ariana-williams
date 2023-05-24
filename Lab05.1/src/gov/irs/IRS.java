eb85b2b7e901a51609287689e7555dac61356974
Ariana Williams <Ariana.Williams@tlgcohort.com>
2023-05-24 09:10:01 -0600

    no changes keeping in sync

--- Lab05.1/src/gov/irs/IRS.java
+++ Lab05.1/src/gov/irs/IRS.java
@@ -3,6 +3,7 @@ package gov.irs;
 public interface IRS {
 
     public void collectTaxes();
+
     public void register(TaxPayer payer);
 
     // static factory method for clients to obtain the SOLE instance of IRSEnum
